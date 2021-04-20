c6f2d9cf2e2d836b3879c9da2a52e0de581b9993
yuriysurzhikov <surzhickov.yura@yandex.ua>
2021-04-20 23:13:11 +0300

    Merge branch 'develop' of D:\Study\Course3\Half2\Android\AutoBroker with conflicts.

--- app/src/main/java/com/yuriysurzhikov/autobroker/di/SyncModule.kt
+++ app/src/main/java/com/yuriysurzhikov/autobroker/di/SyncModule.kt
@@ -7,12 +7,10 @@ import com.yuriysurzhikov.autobroker.model.local.FuelTypeCache
 import com.yuriysurzhikov.autobroker.model.local.GearboxTypeCache
 import com.yuriysurzhikov.autobroker.model.local.RegionRoom
 import com.yuriysurzhikov.autobroker.repository.core.ISynchronizer
-import com.yuriysurzhikov.autobroker.repository.core.IUserLocalRepository
+import com.yuriysurzhikov.autobroker.repository.sync.SynchronizerImpl
 import com.yuriysurzhikov.autobroker.repository.sync.FirebaseSyncRepository
 import com.yuriysurzhikov.autobroker.repository.sync.LocalSyncRepository
 import com.yuriysurzhikov.autobroker.repository.local.SyncDatabase
-import com.yuriysurzhikov.autobroker.repository.remote.UserFirebaseRepository
-import com.yuriysurzhikov.autobroker.repository.sync.SynchronizerImpl
 import com.yuriysurzhikov.autobroker.util.IEntityMapper
 import dagger.Module
 import dagger.Provides
@@ -32,31 +30,21 @@ object SyncModule {
 
     @Provides
     @Singleton
-    fun provideLocalSyncRepository(
-        syncRegionDao: SyncDatabase,
-        fuelEntityMapper: IEntityMapper<FuelType, FuelTypeCache>,
-        regionsEntityMapper: IEntityMapper<Region, RegionRoom>,
-        gearBoxEntityMapper: IEntityMapper<GearboxType, GearboxTypeCache>
+    fun provideLocalSyncRepository(syncRegionDao: SyncDatabase,
+                                   fuelEntityMapper: IEntityMapper<FuelType, FuelTypeCache>,
+                                   regionsEntityMapper: IEntityMapper<Region, RegionRoom>,
+                                   gearBoxEntityMapper: IEntityMapper<GearboxType, GearboxTypeCache>
     ): LocalSyncRepository {
-        return LocalSyncRepository(
-            syncRegionDao,
-            fuelEntityMapper,
-            regionsEntityMapper,
-            gearBoxEntityMapper
-        )
+        return LocalSyncRepository(syncRegionDao, fuelEntityMapper, regionsEntityMapper, gearBoxEntityMapper)
     }
 
     @Provides
     @Singleton
     fun provideSynchronizer(
-        userRepositoryFirebase: UserFirebaseRepository,
-        userRepositoryLocal: IUserLocalRepository,
         firebaseSyncRepository: FirebaseSyncRepository,
         localSyncRepository: LocalSyncRepository
     ): ISynchronizer {
         return SynchronizerImpl(
-            userRepositoryFirebase,
-            userRepositoryLocal,
             firebaseSyncRepository,
             localSyncRepository
         )
